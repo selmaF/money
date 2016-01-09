c965db2750ca992eaa259d9917bf997ebbf938b3
rodstar97 <hermitmovie@googlemail.com>
2016-01-09 01:42:39 +0100

    Muenzen jetzt Verschiebar Goal Widget auf Cannvas

--- firstSteps/coin_game.py
+++ firstSteps/coin_game.py
@@ -2,52 +2,103 @@ from kivy.uix.widget import Widget
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.app import App
 from kivy.graphics import Line, Color
-from kivy.properties import (ObjectProperty, BooleanProperty,NumericProperty)
-from random import random
+from kivy.properties import (ObjectProperty, BooleanProperty, NumericProperty, ListProperty)
+from random import random, randint
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+
+
+Window.size = (768, 1024)
+
+
+class Goal(Widget):
+    pass
+
+
 
 
 class Coin(Widget):
     counter = NumericProperty()
+    worth = NumericProperty()
+
+    def set_coin(self):
+        coin_worthes = [1,2,5,10,20,50,100,200]
+        posible_coins = {1 : 0.7, 2 : 0.75, 5 : 0.8, 10 : 0.7, 20 : 0.75, 50 : 1.0, 100 : 0.93, 200 : 1.09}
+        random_coin = randint(0, 7)
+
+        self.worth = coin_worthes[random_coin]#.get(1,'not found')#randint(1,8)
+        self.size = self.size[0]*(posible_coins.get(coin_worthes[random_coin])), self.size[1]*(posible_coins.get(coin_worthes[random_coin]))
+        print posible_coins.get(coin_worthes[random_coin],1)
+        return self
 
 
 
 class Game(RelativeLayout):
     counter = NumericProperty(0)
+    selected = ObjectProperty(None)
+    sel = BooleanProperty(False)
+    coins = ListProperty()
 
+
+# toDo create random Coins but not on the Borders and no self Intersection
 
     def generate_random_coins(self):
+        generated_coins = []
         for i in range(12):
             self.counter += 1
-            ball = Coin()
-            print self.size
-            center = int(random()*self.size[0]), int(random()*self.size[1])
-            print center
-            ball.center = center
-            ball.counter = self.counter
-            self.add_widget(ball)
+            coin = Coin()
+            proxy_coin_size = coin.size * 2
+            #coinPos = int(random()*Window.size[0])+(coin.size[0]/2), int(random()*Window.size[1]/3.5+(coin.size[1]/2))
+            coin_pos_X = randint(coin.size[0],Window.size[0]-coin.size[0])
+            coin_pos_Y = randint(coin.size[0], int(Window.size[1]/3.5-(coin.size[1])))
+            coin.set_coin()
+            coin.center = coin_pos_X, coin_pos_Y
+            coin.counter = self.counter
+            generated_coins.append(coin)
 
-
-
-    def on_touch_down(self,touch):
-        #if touch.x<100 and touch.y<100: why ?
-            #return super(Ex46, self).on_touch_down(touch) why ?
-        print touch.x, touch.y
-
-        #for child in self.children:
-           # print child.ids['id01'].text
+        for coin in generated_coins:
+            self.add_widget(coin)
+        self.coins = generated_coins
 
 
 
 
-    def on_touch_up(self,touch):
-        self.counter += 1
-        ball = Coin()
-        ball.center = touch.pos
-        ball.counter = self.counter
-        print ball.counter
-        print touch.x, touch.y
-        self.add_widget(ball)
 
+
+    def on_touch_down(self, touch):
+        for child in self.coins:
+            if child.collide_point(*touch.pos):
+                print child.id
+                self.selected = child
+                self.sel = True
+                with self.canvas:
+                    Color(0,0,0)
+                    touch.ud['line'] = Line(rectangle=(child.x-5,child.y-5,child.width+10,child.height+10), dash_length=5, dash_offset=2)
+                    break
+
+        # if touch.x<100 and touch.y<100: why ?
+        # return super(Ex46, self).on_touch_down(touch) why ?
+        #print touch.x, touch.y
+
+        # for child in self.children:
+        # print child.ids['id01'].text
+
+
+    def on_touch_move(self, touch):
+
+        if self.sel == True:
+            self.canvas.remove(touch.ud['line'])
+            child = self.selected
+            child.center = touch.pos
+            #print self.ids
+            with self.canvas:
+                touch.ud['line']=Line(rectangle=(child.x-5,child.y-5,child.width+10,child.height+10),width=1, dash_length=5,dash_offset=2)
+
+    def on_touch_up(self, touch):
+        if self.sel == True:
+            self.canvas.remove(touch.ud['line'])
+            self.sel = False
 
 
 class CoinApp(App):
@@ -55,7 +106,22 @@ class CoinApp(App):
         self.title = 'Widget Selection'
         game = Game()
         game.generate_random_coins()
+        #Clock.schedule_interval(game.update, 1.0/60)
         return game
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     CoinApp().run()
+
+
+
+'''
+# Add first coin on middle spot with random offset
+self.counter += 1
+first_coin = Coin()
+first_coin_pos =int(Window.size[0]/2), int(Window.size[1]/3)
+print type(first_coin_pos)
+first_coin.center = first_coin_pos
+first_coin.counter = self.counter
+self.add_widget(first_coin)
+'''
